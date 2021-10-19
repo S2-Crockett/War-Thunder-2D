31537a4f22274d5ed187e27be8b027e6b8422259
S2-Crockett <Samuel2.Crockett@live.uwe.ac.uk>
2021-10-19 15:05:43 +0100

    Working Movement / bullets

---
+++ War Thunder 2D/Assets/Sam C Assets/Scripts/Bullet.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        transform.rotation = player.transform.rotation;
+        transform.position = player.transform.position;
+        rb.velocity = transform.up * 40;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if(collision.gameObject.tag == "Enemy")
+        {
+            Destroy(this);
+            Destroy(collision.gameObject);
+        }
+
+    }
+}
---
+++ War Thunder 2D/Assets/Sam C Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float rotSpeed;
+    public float speed;
+    private Rigidbody2D rb;
+    public Vector3 eulerAngles;
+    float playerRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        rb.velocity = transform.up * speed;
+        float angle = Mathf.Atan2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Mathf.Rad2Deg;
+        float rotation;
+
+        
+
+        print(playerRotation);
+
+        if (angle != 0)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, -angle), Time.deltaTime * rotSpeed);
+        }
+
+        
+    }
+}
