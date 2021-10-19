24f5daa00fea7c1c4d01a02ab7dfb95c64c31d58
Sam Stamp <samstamp6@gmail.com>
2021-10-19 15:04:06 +0100

    Fixed enemy spawning and destruction issues

---
+++ War Thunder 2D/Assets/Developers/Lewis Assets/Scripts/Enemy.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector3 screenBounds;
+    public Camera cam;
+
+    public float spawnDir;
+
+    private float speed = 2.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //rb.velocity = new Vector2(Random.Range(-3, 3), Random.Range(-3, 3));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Removes Enemies from game if too far away or off of the screen.
+        //Change to just redirect enemies to a different position once off the screen.
+        if (transform.position.x < cam.transform.position.x - 10)
+        {
+            Destroy(this.gameObject);
+            // once player is off the screen rotate back towards the player with a random offset and carry on in 
+            // that direction
+        }
+        if (transform.position.x > cam.transform.position.x + 10)
+        {
+            Destroy(this.gameObject);
+        }
+        if (transform.position.y < cam.transform.position.y - 8)
+        {
+            Destroy(this.gameObject);
+        }
+        if (transform.position.y > cam.transform.position.y + 8)
+        {
+            Destroy(this.gameObject);
+        }
+        
+        
+        if (rb.velocity.x == 0 || rb.velocity.y == 0)
+        {
+            // x will always be 0 to head towards the player, y will have a random offset
+            rb.velocity = new Vector2(spawnDir, Random.Range(-0.5f, 0.5f)) * speed;
+        }
+    }
+}
---
+++ War Thunder 2D/Assets/Developers/Lewis Assets/Scripts/deployPlanes.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deployPlanes : MonoBehaviour
+{
+    public GameObject planePrefab;
+    public float respawnTime = 1.0f;
+    private Vector3 screenBounds;
+    private Camera cam;
+
+    public int numPlanesSpawning;
+    public int numPlanesMax;
+    
+    public GameObject player;
+
+    private Vector3 leftSpawnPos = new Vector3(0,0,0);
+    private Vector3 rightSpawnPos = new Vector3(0,0,0);
+    
+    void Start()
+    {
+        cam = Camera.main;
+        StartCoroutine(planeWave());
+    }
+    private void Update()
+    {
+        // screen width is hardcoded to 8, need to change this if we ever change the screen size.
+        leftSpawnPos = new Vector3(cam.transform.position.x - 8 , cam.transform.position.y,0);
+        rightSpawnPos = new Vector3(cam.transform.position.x + 8 , cam.transform.position.y, 0);
+    }
+    private void spawnPlane()
+    {
+        GameObject plane = Instantiate(planePrefab) as GameObject;
+        plane.GetComponent<Enemy>().cam = cam;
+        plane.GetComponent<Enemy>().spawnDir = 1;
+        plane.transform.position = leftSpawnPos;
+        
+        GameObject plane2 = Instantiate(planePrefab) as GameObject;
+        plane2.GetComponent<Enemy>().cam = cam;
+        plane2.GetComponent<Enemy>().spawnDir = -1;
+        plane2.transform.position = rightSpawnPos;
+
+    }
+    IEnumerator planeWave()
+    {
+        while (true) // Can be changed once main menu is created so we know when to start the game.
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnPlane();
+        }
+    }
+    
+}
